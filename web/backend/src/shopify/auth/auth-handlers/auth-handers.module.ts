6b03711812a096151d36be57064ea8819dce9751
Luke Clifton <luke@propelagency.uk>
2023-07-03 17:53:18 +0100

    Linting

--- web/backend/src/shopify/auth/auth-handlers/auth-handers.module.ts
+++ web/backend/src/shopify/auth/auth-handlers/auth-handers.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { AfterAuthHandler } from './after-auth.handler';
+import { Module } from '@nestjs/common'
+import { AfterAuthHandler } from './after-auth.handler'
 
 @Module({
   providers: [AfterAuthHandler],
