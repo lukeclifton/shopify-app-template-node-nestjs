6d405af1104584e303d739f9952ab8fb63877875
Luke Clifton <luke@propelagency.uk>
2023-07-03 11:24:41 +0100

    Implementing shopify-api and nestjs-shopify packages

--- web/backend/src/app.module.ts
+++ web/backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShopifyCoreModule } from '@nestjs-shopify/core';
+import { ApiVersion } from '@shopify/shopify-api';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ShopifyCoreModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          apiKey: configService.get('SHOPIFY_API_KEY'),
+          apiSecretKey: configService.get('SHOPIFY_API_SECRET'),
+          apiVersion: ApiVersion.Unstable,
+          hostName: configService.get('HOST').replace(/https:\/\//, ''),
+          isEmbeddedApp: true,
+          scopes: configService.get('SHOPIFY_SCOPES').split(','),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
